84ac02920a53de279c335c52c267c74be82f921a
Drew <awoldag@uw.edu>
2026-07-13 13:00:28 -0700

    Basic implementation of a disk writer that outputs as .npy files.

--- src/hyrax_alerts/writers/disk_writer.py
+++ src/hyrax_alerts/writers/disk_writer.py
@@ -1,8 +1,18 @@
+import datetime
+import os
+
+import numpy as np
+
 from hyrax_alerts.writers.base_writer import HyraxAlertsBaseWriter
 
+# NOTE: This is the most naive implementation of a disk writer possible. We need
+# more input from our users to determine specifically what would be the most usable
+# form of the results. For now, simply saving object id and model output in .npy
+# files.
+
 # NOTE: Hyrax already has a mode where it will output results to disk from the
-# infer_stream into a timestamped results directory. However, it doesn't save
-# the source data. This writer _might_ ultimately do that.
+# infer_stream into a timestamped results directory in lance format. However, it
+# doesn't save the source data. This writer _might_ ultimately do that.
 
 
 class HyraxAlertsDiskWriter(HyraxAlertsBaseWriter):
@@ -14,20 +24,27 @@ class HyraxAlertsDiskWriter(HyraxAlertsBaseWriter):
         if self.output_location is None:
             raise ValueError("HyraxAlertsDiskWriter requires an 'output_location' path.")
 
+        # create timestamped output directory within the specified output location
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        self.output_location = os.path.join(self.output_location, f"results_{timestamp}")
+
         # create self.output_location directory if it doesn't exist, log a warning
         # if we create one for the user
-        import os
-
         if not os.path.exists(self.output_location):
             os.makedirs(self.output_location, exist_ok=True)
             print(f"Warning: Created directory '{self.output_location}' for output.")
 
-        # TODO: We should just instantiate a ResultDataset here
+        self.file_counter = 0
 
-    def write(self, data_batch: list, result_batch: list):
+    def write(self, data_batch: dict, result_batch: list | dict[str, list]):
         """Write a batch of results and their source data to disk."""
-        # TODO: Output using ResultDataset class here
-        pass
+        if isinstance(result_batch, dict):
+            result_batch["_object_id"] = data_batch["object_id"]
+        else:
+            result_batch = {"_object_id": data_batch["object_id"], "result": result_batch}
+
+        np.save(f"{self.output_location}/batch_{self.file_counter:08d}.npy", result_batch)
+        self.file_counter += 1
 
     def __enter__(self):
         """Enter the context manager for the writer."""
