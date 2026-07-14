686e694eafecd15f65f5bae2ef3d5e8ef63b287d
drewoldag <47493171+drewoldag@users.noreply.github.com>
2026-07-14 15:57:22 -0700

    Initial implementation of slack bot writer for processed alert output.

--- src/hyrax_alerts/writers/__init__.py
+++ src/hyrax_alerts/writers/__init__.py
@@ -1,3 +1,4 @@
 from .disk_writer import HyraxAlertsDiskWriter
+from .slack_writer import HyraxAlertsSlackWriter
 
-__all__ = ["HyraxAlertsDiskWriter"]
+__all__ = ["HyraxAlertsDiskWriter", "HyraxAlertsSlackWriter"]
