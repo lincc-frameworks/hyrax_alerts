88d044e9431a148767eacf36df89f78f21da41f7
Max West <maxwest@uw.edu>
2026-07-21 13:59:31 -0700

    pin babamul version

--- src/hyrax_alerts/consumers/babamul_consumer.py
+++ src/hyrax_alerts/consumers/babamul_consumer.py
@@ -37,9 +37,10 @@ class BabamulConsumer(HyraxKafkaConsumer):
         dict
             The decoded message as a dictionary.
         """
-        try:
-            from babamul.avro import deserialize_alert
-        except ImportError as err:
-            raise ImportError("babamul package is not installed") from err
+        # try:
+        from babamul.avro import deserialize_alert
+
+        # except ImportError as err:
+        #     raise ImportError("babamul package is not installed") from err
         alert = deserialize_alert(msg.value())
         return alert
