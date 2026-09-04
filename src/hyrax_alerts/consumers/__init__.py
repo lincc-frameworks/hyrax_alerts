bac0c6b01b43b234d2ed9e69c6e001ee1bf1bd97
Max West <maxwest@uw.edu>
2026-09-04 13:43:53 -0700

    default config and __init__ fixes

--- src/hyrax_alerts/consumers/__init__.py
+++ src/hyrax_alerts/consumers/__init__.py
@@ -1,7 +1,7 @@
 from .babamul_consumer import BabamulConsumer, BabamulPhotometryConsumer
 from .base_consumer import HyraxAlertsBaseConsumer
 from .kafka_consumer import HyraxKafkaConsumer
-from .photometry_consumer import TempoPhotometryConsumer
+from .tempo_consumer import TempoPhotometryConsumer
 
 __all__ = [
     "HyraxAlertsBaseConsumer",
