96b8238df3a70afc6e5875406a8f3677a068f2de
Max West <maxwest@uw.edu>
2026-09-04 13:29:49 -0700

    tempo consumer init

--- src/hyrax_alerts/consumers/__init__.py
+++ src/hyrax_alerts/consumers/__init__.py
@@ -1,5 +1,12 @@
 from .babamul_consumer import BabamulConsumer, BabamulPhotometryConsumer
 from .base_consumer import HyraxAlertsBaseConsumer
 from .kafka_consumer import HyraxKafkaConsumer
+from .photometry_consumer import TempoPhotometryConsumer
 
-__all__ = ["HyraxAlertsBaseConsumer", "HyraxKafkaConsumer", "BabamulConsumer", "BabamulPhotometryConsumer"]
+__all__ = [
+    "HyraxAlertsBaseConsumer",
+    "HyraxKafkaConsumer",
+    "BabamulConsumer",
+    "BabamulPhotometryConsumer",
+    "TempoPhotometryConsumer",
+]
